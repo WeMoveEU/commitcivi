d0f6953d8f604c8d9045485336347ffdf9cb9f5d
scardinius <scardinius@chords.pl>
2017-10-19 13:34:04 +0200

    Call for contact id by WemoveContact api

--- CRM/Commitcivi/EventProcessor.php
+++ CRM/Commitcivi/EventProcessor.php
@@ -2,8 +2,20 @@
 
 class CRM_Commitcivi_EventProcessor {
   public static function run(CRM_Commitcivi_Model_Event $event) {
-    // todo create contact
-    $contactId = 0;
+    $params = [
+      'firstname' => $event->contact->firstname,
+      'lastname' => $event->contact->lastname,
+      'email' => $event->contact->email,
+      'postal_code' => $event->contact->postalCode,
+      'country' => $event->contact->country,
+      'action_technical_type' => $event->actionTechnicalType,
+      'action_type' => $event->actionType,
+      'action_name' => $event->actionName,
+      'create_dt' => $event->createDate,
+      'external_id' => $event->externalId,
+    ];
+    $result = civicrm_api3('WemoveContact', 'create', $params);
+    $contactId = $result['id'];
 
     switch ($event->donation->paymentProcessor) {
       case 'sepa':
---
+++ api/v3/WemoveContact.php
@@ -0,0 +1,7 @@
+<?php
+function _civicrm_api3_wemove_contact_create_spec($params) {
+}
+
+function civicrm_api3_wemove_contact_create($params) {
+
+}
