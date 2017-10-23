ea79e96d180c65060b495e6de24aa2a790969997
scardinius <scardinius@chords.pl>
2017-10-23 13:23:53 +0200

    Add class for caching countries

---
+++ CRM/Commitcivi/Logic/Country.php
@@ -0,0 +1,32 @@
+<?php
+
+class CRM_Commitcivi_Logic_Country extends CRM_Commitcivi_Logic_Cache {
+
+  const TYPE_COUNTRY = 'country';
+
+  /**
+   * Get country id by iso code.
+   *
+   * @param int $isoCode ISO code from civicrm_campaign.iso_code
+   *
+   * @return int
+   */
+  public static function getId($isoCode) {
+    if ($cache = self::get(self::TYPE_COUNTRY, 0)) {
+      return CRM_Utils_Array::value($isoCode, $cache[self::TYPE_COUNTRY], 0);
+    }
+    $params = array(
+      'sequential' => 1,
+      'return' => 'id,iso_code',
+      'options' => array('limit' => 0),
+    );
+    $result = civicrm_api3('Country', 'get', $params);
+    $countries = array();
+    foreach ($result['values'] as $country) {
+      $countries[$country['iso_code']] = $country['id'];
+    }
+    self::set(self::TYPE_COUNTRY, 0, $countries);
+    return CRM_Utils_Array::value($isoCode, $countries, 0);
+  }
+
+}
