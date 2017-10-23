3792a30db1bbb7aee07062cb30b7b2c1a1a1d576
scardinius <scardinius@chords.pl>
2017-10-23 11:33:31 +0200

    Fix return in campaign api

--- api/v3/WemoveCampaign.php
+++ api/v3/WemoveCampaign.php
@@ -31,10 +31,10 @@ function civicrm_api3_wemove_campaign_set($params) {
   $campaignObj = new CRM_Commitcivi_Logic_Campaign();
   $campaign = $campaignObj->get($params['external_id'], FALSE, FALSE);
   if ($campaignObj->isValidCampaign($campaign)) {
-    civicrm_api3_create_success($campaign, $params);
+    return civicrm_api3_create_success([$campaign['id'] => $campaign], $params);
   }
   else {
     $campaign = $campaignObj->set($params);
-    civicrm_api3_create_success($campaign, $params);
+    return civicrm_api3_create_success([$campaign['values'][0]['id'] => $campaign['values'][0]], $params);
   }
 }
