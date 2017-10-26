c8b7294752c3656e16be3ca891e566927a29c693
scardinius <scardinius@chords.pl>
2017-10-26 09:48:36 +0200

    Fix donation test class name

--- tests/phpunit/CRM/Commitcivi/DonationTest.php
+++ tests/phpunit/CRM/Commitcivi/DonationTest.php
@@ -12,7 +12,7 @@ require_once 'BaseTest.php';
  *
  * @group headless
  */
-class CRM_Commitcivi_EventProcessorTest extends CRM_Commitcivi_BaseTest {
+class CRM_Commitcivi_DonationTest extends CRM_Commitcivi_BaseTest {
 
   public function testCreateDefaultDonation() {
     $event = new CRM_Commitcivi_Model_Event($this->paramsOneOffStripe);
