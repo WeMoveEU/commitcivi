6c209a1a6f1dbfff3a938d3a52fd4bca83cc7481
scardinius <scardinius@chords.pl>
2017-10-31 14:39:24 +0100

    Support both convention in donation event

--- CRM/Commitcivi/Model/Donation.php
+++ CRM/Commitcivi/Model/Donation.php
@@ -15,18 +15,33 @@ class CRM_Commitcivi_Model_Donation {
   public $bank = '';
 
   public function __construct($params) {
-    $this->amount = $params->donation->amount;
-    $this->amountCharged = $params->donation->amount_charged;
-    $this->currency = $params->donation->currency;
-    $this->cardType = property_exists($params->donation, 'card_type') ? $params->donation->card_type : $this->cardType;
-    $this->paymentProcessor = $params->donation->payment_processor;
-    $this->transactionId = property_exists($params->donation, 'transaction_id') ? $params->donation->transaction_id : $this->transactionId;
-    $this->customerId = property_exists($params->donation, 'customer_id') ? $params->donation->customer_id : $this->customerId;
-    $this->status = $params->donation->status;
-    $this->iban = property_exists($params->donation, 'iban') ? $params->donation->iban : $this->iban;
-    $this->bic = property_exists($params->donation, 'bic') ? $params->donation->bic : $this->bic;
-    $this->accountHolder = property_exists($params->donation, 'account_holder') ? $params->donation->account_holder : $this->accountHolder;
-    $this->bank = property_exists($params->donation, 'bank') ? $params->donation->bank : $this->bank;
+    $donation = $this->get($params);
+    $this->amount = $donation->amount;
+    $this->amountCharged = $donation->amount_charged;
+    $this->currency = $donation->currency;
+    $this->cardType = property_exists($donation, 'card_type') ? $donation->card_type : $this->cardType;
+    $this->paymentProcessor = $donation->payment_processor;
+    $this->transactionId = property_exists($donation, 'transaction_id') ? $donation->transaction_id : $this->transactionId;
+    $this->customerId = property_exists($donation, 'customer_id') ? $donation->customer_id : $this->customerId;
+    $this->status = $donation->status;
+    $this->iban = property_exists($donation, 'iban') ? $donation->iban : $this->iban;
+    $this->bic = property_exists($donation, 'bic') ? $donation->bic : $this->bic;
+    $this->accountHolder = property_exists($donation, 'account_holder') ? $donation->account_holder : $this->accountHolder;
+    $this->bank = property_exists($donation, 'bank') ? $donation->bank : $this->bank;
+  }
+
+  /**
+   * Get donation object.
+   *
+   * @param object $params
+   *
+   * @return mixed
+   */
+  private function get($params) {
+    if (property_exists($params, 'donation')) {
+      return $params->donation;
+    }
+    return $params->metadata;
   }
 
 }
--- tests/phpunit/CRM/Commitcivi/ModelTest.php
+++ tests/phpunit/CRM/Commitcivi/ModelTest.php
@@ -25,4 +25,41 @@ class CRM_Commitcivi_ModelTest extends CRM_Commitcivi_BaseTest {
     $this->assertEquals('PL', $event->contact->country);
   }
 
+  public function testDonationOneOff() {
+    $event = new CRM_Commitcivi_Model_Event($this->oneOffStripeEvent());
+    $this->assertEquals(15.67, $event->donation->amount, '', 0.001);
+    $this->assertEquals(0.17, $event->donation->amountCharged, '', 0.001);
+    $this->assertEquals('EUR', $event->donation->currency);
+    $this->assertEquals('Visa', $event->donation->cardType);
+    $this->assertEquals('stripe', $event->donation->paymentProcessor);
+    $this->assertEquals('ch_1NHwmdLnnERTfiJAMNHyFjV4', $event->donation->transactionId);
+    $this->assertEquals('cus_Bb94Wds2n3xCVB', $event->donation->customerId);
+    $this->assertEquals('success', $event->donation->status);
+  }
+
+  public function testDonationOneOffInOldStyle() {
+    $event = new CRM_Commitcivi_Model_Event($this->oneOffStripeOldStyleEvent());
+    $this->assertEquals(15.67, $event->donation->amount, '', 0.001);
+    $this->assertEquals(0.17, $event->donation->amountCharged, '', 0.001);
+    $this->assertEquals('EUR', $event->donation->currency);
+    $this->assertEquals('Visa', $event->donation->cardType);
+    $this->assertEquals('stripe', $event->donation->paymentProcessor);
+    $this->assertEquals('ch_1NHwmdLnnERTfiJAMNHyFjV4', $event->donation->transactionId);
+    $this->assertEquals('cus_Bb94Wds2n3xCVB', $event->donation->customerId);
+    $this->assertEquals('success', $event->donation->status);
+  }
+
+  public function testDonationRecurring() {
+    $event = new CRM_Commitcivi_Model_Event($this->recurringSepaEvent());
+    $this->assertEquals(42, $event->donation->amount, '', 0.001);
+    $this->assertEquals(0, $event->donation->amountCharged, '', 0.001);
+    $this->assertEquals('EUR', $event->donation->currency);
+    $this->assertEquals('sepa', $event->donation->paymentProcessor);
+    $this->assertEquals('PL83101010230000261395100000', $event->donation->iban);
+    $this->assertEquals('NBPLPLPWXXX', $event->donation->bic);
+    $this->assertEquals('Holder name', $event->donation->accountHolder);
+    $this->assertEquals('Narodowy Bank Polski, O/Okr. w Warszawie', $event->donation->bank);
+    $this->assertEquals('success', $event->donation->status);
+  }
+
 }
