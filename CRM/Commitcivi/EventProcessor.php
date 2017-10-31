cca7971785762bc67c3f5864ffe9db374da680e3
scardinius <scardinius@chords.pl>
2017-10-31 14:55:59 +0100

    Extract code for campaign and contact to separate methods

--- CRM/Commitcivi/EventProcessor.php
+++ CRM/Commitcivi/EventProcessor.php
@@ -3,14 +3,48 @@
 class CRM_Commitcivi_EventProcessor {
 
   public function process(CRM_Commitcivi_Model_Event $event) {
+    $campaignId = $this->campaign($event);
+    $contactId = $this->contact($event, $campaignId);
+    $donation = new CRM_Commitcivi_Logic_Donation();
+    switch ($event->donation->paymentProcessor) {
+      case 'sepa':
+        $result = $donation->sepa($event, $contactId, $campaignId);
+        break;
+
+      default:
+        $result = $donation->create($event, $contactId, $campaignId);
+        break;
+    }
+
+    return $result['count'];
+  }
+
+  /**
+   * Get campaign Id.
+   *
+   * @param \CRM_Commitcivi_Model_Event $event
+   *
+   * @return mixed
+   */
+  public function campaign(CRM_Commitcivi_Model_Event $event) {
     $params = [
       'action_name' => $event->actionName,
       'external_identifier' => $event->externalIdentifier,
       'campaign_type_id' => CRM_Core_PseudoConstant::getKey('CRM_Campaign_BAO_Campaign', 'campaign_type_id', 'Fundraising'),
     ];
     $result = civicrm_api3('WemoveCampaign', 'create', $params);
-    $campaignId = $result['id'];
+    return $result['id'];
+  }
 
+  /**
+   * Get contact Id.
+   *
+   * @param \CRM_Commitcivi_Model_Event $event
+   * @param $campaignId
+   *
+   * @return mixed
+   */
+  public function contact(CRM_Commitcivi_Model_Event $event, $campaignId) {
     $params = [
       'firstname' => $event->contact->firstname,
       'lastname' => $event->contact->lastname,
@@ -25,20 +59,7 @@ class CRM_Commitcivi_EventProcessor {
       'campaign_id' => $campaignId,
     ];
     $result = civicrm_api3('WemoveContact', 'create', $params);
-    $contactId = $result['id'];
-
-    $donation = new CRM_Commitcivi_Logic_Donation();
-    switch ($event->donation->paymentProcessor) {
-      case 'sepa':
-        $result = $donation->sepa($event, $contactId, $campaignId);
-        break;
-
-      default:
-        $result = $donation->create($event, $contactId, $campaignId);
-        break;
-    }
-
-    return $result['count'];
+    return $result['id'];
   }
 
 }
