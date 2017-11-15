88b4d8ef92ab930d8ea1d4398d53b06d54025af2
scardinius <scardinius@chords.pl>
2017-11-15 16:33:31 +0100

    Test sepa details

--- tests/phpunit/CRM/Commitcivi/DonationTest.php
+++ tests/phpunit/CRM/Commitcivi/DonationTest.php
@@ -21,4 +21,42 @@ class CRM_Commitcivi_DonationTest extends CRM_Commitcivi_BaseTest {
     $this->assertEquals(1, $result);
   }
 
+  public function testSepaDetails() {
+    $event = new CRM_Commitcivi_Model_Event($this->recurringSepaEvent());
+    $contact = civicrm_api3('Contact', 'get', array(
+      'sequential' => 1,
+      'options' => array('limit' => 1, 'sort' => "id DESC"),
+    ));
+    $contactId = $contact['id'];
+    $campaign = civicrm_api3('Campaign', 'get', array(
+      'sequential' => 1,
+      'options' => array('limit' => 1, 'sort' => "id DESC"),
+    ));
+    $campaignId = $campaign['id'];
+
+    $donation = new CRM_Commitcivi_Logic_Donation();
+    $mandate = $donation->sepa($event, $contactId, $campaignId);
+    $mnd = $mandate['values'][0];
+    $contributionRecur = civicrm_api3('ContributionRecur', 'get', array(
+      'sequential' => 1,
+      'id' => $mnd['entity_id'],
+    ));
+    $conrec = $contributionRecur['values'][0];
+
+    $this->assertArrayHasKey('id', $mandate);
+    $this->assertEquals('civicrm_contribution_recur', $mnd['entity_table']);
+    $this->assertEquals($event->actionName, $mnd['source']);
+    $this->assertEquals($event->donation->iban, $mnd['iban']);
+    $this->assertEquals($event->donation->bic, $mnd['bic']);
+    $this->assertEquals('RCUR', $mnd['type']);
+    $this->assertEquals('FRST', $mnd['status']);
+    $this->assertEquals($event->donation->amount, $conrec['amount']);
+    $this->assertEquals($event->donation->currency, $conrec['currency']);
+    $this->assertEquals('month', $conrec['frequency_unit']);
+    $this->assertEquals(1, $conrec['frequency_interval']);
+    $this->assertEquals(2, $conrec['contribution_status_id']);
+    $this->assertEquals(1, $conrec['cycle_day']);
+    $this->assertEquals($campaignId, $conrec['campaign_id']);
+  }
+
 }
