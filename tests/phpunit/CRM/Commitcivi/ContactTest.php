de4dae67a2b81d1904f35a9b187b1c1f99f3c0c9
scardinius <scardinius@chords.pl>
2017-11-15 12:31:05 +0100

    Test create new contact

--- tests/phpunit/CRM/Commitcivi/ContactTest.php
+++ tests/phpunit/CRM/Commitcivi/ContactTest.php
@@ -23,4 +23,13 @@ class CRM_Commitcivi_ContactTest extends CRM_Commitcivi_BaseTest {
     $this->assertGreaterThan(0, $contactId);
   }
 
+  public function testCreateNew() {
+    $event = new CRM_Commitcivi_Model_Event($this->oneOffStripeOldStyleEvent());
+    $event->contact->email = time() . '@speakcivi.com';
+    $processor = new CRM_Commitcivi_EventProcessor();
+    $campaignId = $processor->campaign($event);
+    $contactId = $processor->contact($event, $campaignId);
+    $this->assertGreaterThan(0, $contactId);
+  }
+
 }
