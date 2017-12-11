b30236c3586ed165d188498daed976e214e61ff0
scardinius <scardinius@chords.pl>
2017-12-11 17:27:56 +0100

    Improve utm model

--- CRM/Commitcivi/Model/Utm.php
+++ CRM/Commitcivi/Model/Utm.php
@@ -7,9 +7,9 @@ class CRM_Commitcivi_Model_Utm {
   public $Content = '';
 
   public function __construct($params) {
-    $this->Source = $params->source->source;
-    $this->Medium = $params->source->medium;
-    $this->Campaign = $params->source->campaign;
+    $this->Source = property_exists($params->source, 'source') ? $params->source->source : $params->source->source;
+    $this->Medium = property_exists($params->source, 'medium') ? $params->source->medium : $params->source->medium;
+    $this->Campaign = property_exists($params->source, 'campaign') ? $params->source->campaign :  $params->source->campaign;
     $this->Content = property_exists($params->source, 'content') ? $params->source->content : $this->Content;
   }
 
