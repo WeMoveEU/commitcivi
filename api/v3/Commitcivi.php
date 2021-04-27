8e337ed265325cc248dd0c5280e1f60dbff27193
Aaron Elliot Ross <aaronelliotross@gmail.com>
2021-04-27 14:31:09 +0200

    API function for create a group of new recurring donors in the last N days.

--- api/v3/Commitcivi.php
+++ api/v3/Commitcivi.php
@@ -47,23 +47,76 @@ function civicrm_api3_commitcivi_update_major_donors($params) {
   return civicrm_api3_create_success($returnResult, $params);
 }
 
-function civicrm_api3_commitcivi_new_recurring_donors() {
+function _civicrm_api3_commitcivi_new_recurring_donors_spec(&$spec) {
+  $spec['days'] = [
+    'name' => 'days',
+    'title' => ts('Days Ago'),
+    'description' => 'New recurring donor in the last N days',
+    'type' => CRM_Utils_Type::T_INT,
+    'api.required' => 0,
+    'api.default' => 7,
+  ];
+}
+
+function civicrm_api3_commitcivi_new_recurring_donors($params) {
   # create a new group for this week
   $date = new DateTime();
-  $date->modify('-1 week');
-  $group = civicrm_api3('Group', 'create', ['name' => "New recurring donors, week of {$date->format('Y-m-d')}"]);
+  $date->modify("-{$params['days']} day");
+
+  $group_name = "new-recurring-donors-{$date->format('Y-m-d')}";
+
+  CRM_Core_DAO::executeQuery(
+    "INSERT IGNORE INTO civicrm_group (name, title, refresh_date) " . 
+    " VALUES ( " .
+    "  '{$group_name}', " .
+    "  'New recurring donors, week of {$date->format('Y-m-d')}', " .
+    "  NOW() " .
+    " )"
+  );
+
+  $result = CRM_Core_DAO::executeQuery(
+    "SELECT * from civicrm_group WHERE name = '{$group_name}'"
+  );
+  $result->fetch();
+  $group = $result->id;
 
   # add new recurring donors for the week to the group
-  $outfile = "new-recurring-donors-{$date->format('Ymd')}.csv";
+  $outfile = "/var/lib/mysql-files/new-recurring-donors-{$date->format('U')}.csv";
+
   CRM_Core_DAO::executeQuery(
-    "SELECT contact_id, {$group->id}, 'Added'" .
+    "SELECT contact_id, {$group}, 'Added'" .
     " INTO OUTFILE '{$outfile}' " .
     " FROM civicrm_contribution_recur " .
-    " WHERE create_date >= {$date->format('Y-m-d 00:00:00')}"
+    " WHERE create_date >= '{$date->format('Y-m-d 00:00:00')}'"
   );
 
   CRM_Core_DAO::executeQuery(
-    "LOAD DATA INFILE '{$filename} IGNORE INTO TABLE civicrm_contact_group (contact_id, group_id, status)"
-
+    "LOAD DATA INFILE '{$outfile}' " .
+    "INTO TABLE " . 
+    "civicrm_group_contact (contact_id, group_id, status)"
   );
+}
+
+
+function civicrm_api3_commitcivi_clean_new_recurring_donor_groups() {
+  # find groups to clear out
+  $date = new DateTime();
+  $date->modify('-4 weeks');
+
+  $result = CRM_Core_DAO::executeQuery(
+    "SELECT id FROM civicrm_group " . 
+    "WHERE name like 'new-recurring-donors-%' " .
+    "AND refresh_date < '{$date->format('Y-m-d')}' "
+  );
+
+  while ($result->fetch()) {
+    CRM_Core_DAO::executeQuery(
+      "DELETE FROM civicrm_group_contact " .
+      "WHERE group_id = {$result->id}"
+    );
+    CRM_Core_DAO::executeQuery(
+      "DELETE FROM civicrm_group " .
+      "WHERE id = {$result->id}"
+    );
+  }
 }
