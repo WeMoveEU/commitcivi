ebdb7e74db371a734c4b3b27b4cb7ff448459643
Aaron Elliot Ross <aaronelliotross@gmail.com>
2021-05-03 11:46:57 +0200

    Look up the recurring donation with the recurring id.

--- CRM/Commitcivi/Logic/StripeMigration.php
+++ CRM/Commitcivi/Logic/StripeMigration.php
@@ -2,8 +2,6 @@
 
 class CRM_Commitcivi_Logic_StripeMigration
 {
-
-
     /**
      * @param \CRM_Commitcivi_Model_Event $event
      *
@@ -12,50 +10,42 @@ class CRM_Commitcivi_Logic_StripeMigration
      */
     public function migrate(CRM_Commitcivi_Model_Event $event)
     {
-        $contact = $event->contact;
+        // $contact = $event->contact;
         $donation = $event->donation;
+        $recurringId = $event->donation->recurringId;
 
-        # find the current houdini recurring donation using email, processor, amount and start_date
-        $query = ""
-            . "SELECT recur.id recurring_id, contact.id contact_id "
-            . " FROM civicrm_contribution_recur recur "
-            . " JOIN civicrm_contact contact ON (contact.id=recur.contact_id) "
-            . " JOIN civicrm_email email ON (contact.id=email.contact_id AND email.is_primary)"
-            . "WHERE email.email = %1 "
-            . "AND payment_processor_id = 11 "
-            . "AND amount * 100 = %2 "  # units are 1s in Civi (3 euros) and 100ths (300) in Houdini 
-            . "AND left(start_date, 10) = %3 ";
+        $civicrm_recurring = civicrm_api3(
+            'ContributionRecur',
+            'get',
+            [
+                'sequential' => 1,
+                'trxn_id' => $recurringId,
+            ]
+        );
 
-        $query_params = [
-            '1' => [$contact->email, 'String'],
-            '2' => [$donation->amount, 'Float'],
-            '3' => [$donation->stripeRecurringStart, 'String']
-        ];
-
-        $result = CRM_Core_DAO::executeQuery($query, $query_params);
-        $migrated = 0;
-
-        while ($result->fetch()) {
-            civicrm_api3('StripeSubscription', 'import', [
-                'subscription_id' => $donation->stripeSubscriptionId,
-                'contact_id' => $result->contact_id,
-                'payment_processor_id' => CRM_Commitcivi_Logic_Settings::paymentProcessorIdCard(),
-            ]);
-            civicrm_api3(
-                'ContributionRecur',
-                'cancel',
-                [
-                    'id' => $result->recurring_id,
-                    'cancel_reason' => "Migrated to Stripe {$donation->stripeSubscriptionId}"
-                ]
-            );
-
-            ++$migrated;
+        if (!$civicrm_recurring) {
+            $event_as_json = json_encode($event);
+            CRM_Core_Error::fatal("Couldn't find a recurring donation for recurringId {$recurringId} {$event_as_json}");
         }
 
-        //
-        // How do we report a warning without exiting the consumer?
-        //
-        return $migrated == 0 ? "Didn't find any recurring donations to migrate." : 1;
+        civicrm_api3(
+            'StripeSubscription',
+            'import',
+            [
+                'subscription_id' => $donation->stripeSubscriptionId,
+                'contact_id' => $civicrm_recurring->contactId,
+                'payment_processor_id' => CRM_Commitcivi_Logic_Settings::paymentProcessorIdCard(),
+            ]
+        );
+        civicrm_api3(
+            'ContributionRecur',
+            'cancel',
+            [
+                'id' => $civicrm_recurring->id,
+                'cancel_reason' => "Migrated to Stripe {$donation->stripeSubscriptionId}"
+            ]
+        );
+
+        return 1;
     }
 }
