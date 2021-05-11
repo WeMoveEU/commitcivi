72b0019baabc4bb977f2b59973151cca2d276db0
Aaron Elliot Ross <aaronelliotross@gmail.com>
2021-05-11 16:09:04 +0200

    Handle many more "corner" cases and workaround a bug in StripeSubscription.import

--- CRM/Commitcivi/Logic/StripeMigration.php
+++ CRM/Commitcivi/Logic/StripeMigration.php
@@ -8,9 +8,12 @@ class CRM_Commitcivi_Logic_StripeMigration {
      * @throws \CiviCRM_API3_Exception
      */
     public function migrate(CRM_Commitcivi_Model_Event $event) {
-        // $contact = $event->contact;
+        $contact = $event->contact;
         $donation = $event->donation;
         $recurringId = $event->donation->recurringId;
+        $event_as_json = json_encode($event);
+
+        CRM_Core_Error::debug_log_message("Event received : {$event_as_json}");
 
         $civicrm_recurring = civicrm_api3(
             'ContributionRecur',
@@ -21,16 +24,24 @@ class CRM_Commitcivi_Logic_StripeMigration {
             ]
         );
 
-        if (!$civicrm_recurring) {
-            $event_as_json = json_encode($event);
-            CRM_Core_Error::fatal("Couldn't find a recurring donation for recurringId {$recurringId} {$event_as_json}");
+        if ($civicrm_recurring['count'] == 0) {
+            CRM_Core_Error::debug_log_message(
+                "[This is ok, just letting you know] " .
+                "Couldn't find a recurring donation for recurringId {$recurringId} {$event_as_json}"
+            );
         }
 
-        $contactId = $civicrm_recurring['values'][0]['contact_id'];
-        $date = date('Y-m-d');
+        if ($civicrm_recurring['count'] > 0) {
+            $recurring_donation = $civicrm_recurring['values'][0];
+            $contactId = $recurring_donation['contact_id'];
+        }
+
+        if (!$contactId) {
+            $contactId = $this->find_contact_by_email($contact->email, $contact->firstname, $contact->lastname);
+        }
 
         CRM_Core_Error::debug_log_message(
-            "Importing Stripe subscription {$donation->stripeSubscriptionId} for civcrm_contact {$contactId}"
+            "Importing Stripe subscription {$donation->stripeSubscriptionId} for civicrm_contact {$contactId}"
         );
 
         # Handling the customer here is a workaround for a bug in the StripeSubscription.import
@@ -46,30 +57,77 @@ class CRM_Commitcivi_Logic_StripeMigration {
             $customer = civicrm_api3('StripeCustomer', 'create', $customerParams);
         }
 
-        $results = civicrm_api3(
-            'StripeSubscription',
-            'import',
-            [
-                'subscription_id' => $donation->stripeSubscriptionId,
-                'contact_id' => $contactId,
-                'payment_processor_id' => 1, # Live Stripe Account
-                'contribution_source' => "Migrated from CommitChange {$date}",
+        try {
+            $date = date('Y-m-d');
+            $results = civicrm_api3(
+                'StripeSubscription',
+                'import',
+                [
+                    'subscription_id' => $donation->stripeSubscriptionId,
+                    'contact_id' => $contactId,
+                    'payment_processor_id' => 1, # Live Stripe Account
+                    'contribution_source' => "Migrated from CommitChange {$date}",
 
-            ]
-        );
+                ]
+            );
+        }
+        catch (CiviCRM_API3_Exception $e) {            
+            if (preg_match('/Found matching recurring contribution/', $e->getMessage())) {
+                CRM_Core_Error::debug_log_message("[This is OK, just letting you know] {$e->getMessage()}");
+                # keep going to make sure the previous recurring donation is cancelled
+            }
+            else {
+                throw $e;
+            }
+        }
 
         $debug_results = json_encode($results);
         CRM_Core_Error::debug_log_message("Migrated recurring donation to {$debug_results}");
 
-        civicrm_api3(
-            'ContributionRecur',
-            'cancel',
+        if ($recurring_donation['id']) {
+            CRM_Core_Error::debug_log_message("Cancelling {$recurring_donation['id']}");
+
+            civicrm_api3(
+                'ContributionRecur',
+                'cancel',
+                [
+                    'id' => $recurring_donation['id'],
+                    'cancel_reason' => "Migrated to Stripe {$donation->stripeSubscriptionId}"
+                ]
+            );
+        }
+
+        return 1;
+    }
+
+    private function find_contact_by_email($email, $first, $last) {
+        CRM_Core_Error::debug_log_message("Looking for contact using email: {$email}");
+        # "contact":{"language":"","firstname":"","lastname":"","emails":[{"email":"user@example.com"}]
+        $contact = civicrm_api3(
+            'Email',
+            'get',
             [
-                'id' => $civicrm_recurring['id'],
-                'cancel_reason' => "Migrated to Stripe {$donation->stripeSubscriptionId}"
+                'sequential' => 1,
+                array('sort' => "id DESC"),
+                'is_primary' => 1,
+                'email' => $email
             ]
         );
 
-        return 1;
+        if ($contact['count'] == 0) {
+            $contact = civicrm_api3(
+                'Contact',
+                'create',
+                [ "first_name" => $first, "last_name" => $last,  "contact_type" => "Individual", ]
+            );
+            civicrm_api3(
+                'Email',
+                'create',
+                [ "contact_id" => $contact['id'], "email" => $email, "is_primary" => 1 ]
+            );
+            return $contact['id'];
+        }
+
+        return $contact['values'][0]['contact_id'];
     }
 }
